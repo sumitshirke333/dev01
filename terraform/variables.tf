4d4836b77d4c184c089f7bc2d95012f1336ee292
sumitshirke333 <sumitshirke1821@gmail.com>
2025-11-05 13:47:39 +0000

    Clean commit: only frontend, backend, and Docker setup

--- terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "rg_name" { default = "devops-demo-rg" }
-variable "location" { default = "east" }
-variable "acr_name" { default = "devopsdemoacr123" } 
-variable "name_prefix" { default = "devopsdemo" }
-variable "vm_size" { default = "Standard_B1s" } 
-variable "admin_user" { default = "azureuser" }
-variable "ssh_pub_key_path" { default = "~/.ssh/id_rsa.pub" }
